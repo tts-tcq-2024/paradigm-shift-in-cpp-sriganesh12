f04d1c76986f9fddd14f5e52533a9d8e557d7973
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-08-27 11:13:58 +0530

    Refactor Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -2,18 +2,43 @@
 #include <iostream>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+bool isTemperatureInRange(float temperature)
+{
+  if(temperature < 0 || temperature > 45)
+  {
     cout << "Temperature out of range!\n";
     return false;
-  } else if(soc < 20 || soc > 80) {
+  }
+  return true;
+}
+
+bool isStateOfChargeInRange(float soc)
+{
+  if(soc < 20 || soc > 80) 
+  {
     cout << "State of Charge out of range!\n";
     return false;
-  } else if(chargeRate > 0.8) {
+  }
+  return true;
+}
+
+bool isChargeRateInRange(float chargeRate)
+{
+  if(chargeRate > 0.8) 
+  {
     cout << "Charge Rate out of range!\n";
     return false;
   }
   return true;
+}
+
+bool batteryIsOk(float temperature, float soc, float chargeRate)
+{
+  bool isTemperatureOk{isTemperatureInRange(temperature)};
+  bool isStateOfChargeOk{isStateOfChargeInRange(soc)};
+  bool isChargeRateOk{isChargeRateInRange(chargeRate)};
+
+  return isTemperatureOk && isStateOfChargeOk && isChargeRateOk;
 }
 
 int main() {
