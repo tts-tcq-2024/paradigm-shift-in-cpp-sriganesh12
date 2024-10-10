79e4dd556f15d59ee1875f070516b5cb6f6992f4
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-10-10 11:23:48 +0530

    Update parameters.h

--- parameters.h
+++ parameters.h
@@ -4,10 +4,11 @@
 struct parameters
 {
 float temperatureMax = 45;
-float temperatureMin = 45;
+float temperatureMin = 0;
 float StateOfChargeMax = 80;
 float StateOfChargeMin = 20;
 float ChargeRateMax = 0.8;
+float ChargeRateMin = 0;
 float warningTolerance = 0.05;
 bool warningForTemperature = true;
 bool warningForStateOfCharge = true;
