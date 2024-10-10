32c4f52c666859b49dec88c7ed96479a45531caf
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-10-10 12:01:31 +0530

    Create Test_Checker.cpp

---
+++ Test_Checker.cpp
@@ -0,0 +1,12 @@
+#include "Checker.cpp"
+#include <gtest/gtest.h>
+
+TEST(BatteryCheckTest, Temperature) {
+    EXPECT_TRUE(batteryIsOk(28, 74, 0.6)); // All Parameters in Range
+    EXPECT_FALSE(batteryIsOk(48, 83, 0.4)); // Temperature Breach
+    EXPECT_FALSE(batteryIsOk(-5, 52, 0.6)); // Temperature Breach
+    EXPECT_FALSE(batteryIsOk(30, 10, 0.5)); // Soc Breach
+    EXPECT_FALSE(batteryIsOk(25, 60, 0.85)); // Charge rate Breach
+    EXPECT_TRUE(batteryIsOk(43, 76, 0.6)); // All Params in Range(Temperature and Soc in warning stage)
+    EXPECT_TRUE(batteryIsOk(2, 22, 0.78)); // All Params in Range(Temperature, Soc and ChargeRate in Warning Stage)
+}
