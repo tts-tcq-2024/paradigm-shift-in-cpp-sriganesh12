19d31e4749fa472bc9371d67a4513eb0c87b1e18
sriganesh12 <49653366+sriganesh12@users.noreply.github.com>
2024-10-10 12:10:06 +0530

    Update and rename Test_Checker.cpp to Checker.Test.cpp

--- Checker.Test.cpp
+++ Checker.Test.cpp
@@ -1,5 +1,4 @@
 #include "Checker.cpp"
-#include <gtest/gtest.h>
 
 TEST(BatteryCheckTest, Temperature) {
     EXPECT_TRUE(batteryIsOk(28, 74, 0.6)); // All Parameters in Range
@@ -9,9 +8,4 @@ TEST(BatteryCheckTest, Temperature) {
     EXPECT_FALSE(batteryIsOk(25, 60, 0.85)); // Charge rate Breach
     EXPECT_TRUE(batteryIsOk(43, 76, 0.6)); // All Params in Range(Temperature and Soc in warning stage)
     EXPECT_TRUE(batteryIsOk(2, 22, 0.78)); // All Params in Range(Temperature, Soc and ChargeRate in Warning Stage)
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
